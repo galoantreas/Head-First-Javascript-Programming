8bcc65d496fe581629082ce85c8fb2ab78d42dc5
Antreas Galanos <antreasau79@outlook.com>
2021-06-02 21:30:20 +0300

    Math.random.

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = lokation2 + 1;
 
 var guess;
 var hits = 0;
@@ -15,7 +16,7 @@ while (isSunk == false) {
 		} else {
 			guesses ++ 
 		
-			if (guess == location1 || guess == location2 || guess == 			    location3) {
+			if (guess == location1 || guess == location2 || guess == location3) {
 					alert("HIT!");
 					hits ++;				
 					if (hits == 3) {
